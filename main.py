eed45f0e52ed06155a676623272872c791ac2771
Marcin <marcin.smentek@gamil.com>
2023-08-24 23:02:47 +0200

    dodano kod stron

--- main.py
+++ main.py
@@ -4,35 +4,92 @@ from flask import Flask, render_template, request, url_for, redirect
 from functions import *
 
 
-# api = requests.get('https://api.spoonacular.com/food/wine/dishes?wine=verdicchio&apiKey=358023a4e8234adb908c38e4bb7a13b2')
-# dane = api.json()
-# print(dane)
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    wines = loading_files('tekst/type_of_wines.txt')
 
-    list = request.form.get("lista")
-    if list:
-        if list == "white_wine":
-            type_of_wine = loading_files('tekst/white_wine.txt')
+    return render_template("index.html", )
 
 
+@app.route("/wine", methods=['POST', 'GET'])
+def choise_wine():
 
-    list2 = request.form.get("lista2")
-    list3 = request.form.get("lista3")
-
-    # x2 = lista(list,list2)[0]
-    # x3 = lista(list2,list3)[1]
+    return render_template("choise_wine.html")
 
 
-
-    print(f'@@@{list}')
-
-    context = {"wino": wines,
-               "wino2": type_of_wine
-
+@app.route("/ingredients", methods=['POST', 'GET'])
+def ingredients():
+    ask = request.form.get('ask')
+    print(ask)
+    wynik = get_api(ask)
+    context = {"wynik": wynik,
                }
+    return render_template("igredients.html", context=context)
 
-    return render_template("index.html", context=context)
+@app.route("/wine/white_wine", methods=['POST', 'GET'])
+def white_wine():
+    wines = loading_files('tekst/white_wine.txt')
+    context = {"wino": wines,
+               }
+    return render_template("white_wine.html", context=context)
+@app.route("/wine/white_wine/dry", methods=['POST', 'GET'])
+def white_dry_wine():
+    wines = loading_files('tekst/dry_white_wine.txt')
+    context = {"wino": wines,
+               }
+    return render_template("dry_white_wine.html", context=context)
+
+@app.route("/wine/red_wine", methods=['POST', 'GET'])
+def red_wine():
+    wines = loading_files('tekst/red_wine.txt')
+    context = {"wino": wines,
+               }
+    return render_template("red_wine.html", context=context)
+
+@app.route("/wine/red_wine/dry", methods=['POST', 'GET'])
+def red_dry_wine():
+    wines = loading_files('tekst/dry_red_wine.txt')
+    context = {"wino": wines,
+               }
+    return render_template("dry_red_wine.html", context=context)
+
+@app.route("/wine/desert_wine", methods=['POST', 'GET'])
+def desert_wine():
+    wines = loading_files('tekst/dessert_wine.txt')
+    context = {"wino": wines,
+               }
+    return render_template("desert_wine.html", context=context)
+@app.route("/wine/rose_wine", methods=['POST', 'GET'])
+def rose_wine():
+    wines = loading_files('tekst/rose_wine.txt')
+    context = {"wino": wines,
+               }
+    return render_template("rose_wine.html", context=context)
+@app.route("/wine/sparkling_wine", methods=['POST', 'GET'])
+def sparkling_wine():
+    wines = loading_files('tekst/sparkling_wine.txt')
+    context = {"wino": wines,
+               }
+    return render_template("sparkling_wine.html", context=context)
+@app.route("/wine/sherry_wine", methods=['POST', 'GET'])
+def sherry():
+    wines = loading_files('tekst/sherry.txt')
+    context = {"wino": wines,
+               }
+    return render_template("sherry.html", context=context)
+@app.route("/wine/vermouth", methods=['POST', 'GET'])
+def vermouth():
+    wines = loading_files('tekst/ vermouth.txt')
+    context = {"wino": wines,
+               }
+    return render_template("vermouth.html", context=context)
+
+
+
+
+
+
+
