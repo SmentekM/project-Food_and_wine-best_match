6fcd19e64203206d1b7b7ad420974eb58882af84
Marcin <marcin.smentek@gamil.com>
2023-08-24 23:02:17 +0200

    dodado funkcje get api

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+import json
+import requests
+
+
 def loading_files(file):
     date = []
     with open(file, "r") as f:
@@ -5,4 +9,8 @@ def loading_files(file):
             date.append(idx)
     return date
 
-
+def get_api(ask):
+    api = requests.get(f'https://api.spoonacular.com/food/wine/dishes?wine={ask}&apiKey=358023a4e8234adb908c38e4bb7a13b2')
+    date = api.json()
+    # print(date)
+    return date
