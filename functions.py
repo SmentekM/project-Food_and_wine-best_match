ffebd4b020a59c763a6b8aab090a600fa3b4cb9e
Marcin <marcin.smentek@gamil.com>
2023-08-25 12:50:56 +0200

    dodano fukcje get api mael

--- functions.py
+++ functions.py
@@ -6,11 +6,17 @@ def loading_files(file):
     date = []
     with open(file, "r") as f:
         for idx in f:
-            date.append(idx)
+            date.append(idx.strip())
     return date
 
-def get_api(ask):
+def get_api_igredients(ask):
     api = requests.get(f'https://api.spoonacular.com/food/wine/dishes?wine={ask}&apiKey=358023a4e8234adb908c38e4bb7a13b2')
+    date = api.json()['pairings']
+
+    return date
+
+def get_api_meal(ask):
+    api = requests.get(f'https://api.spoonacular.com/recipes/findByIngredients?ingredients={ask}&number=2&apiKey=358023a4e8234adb908c38e4bb7a13b2')
     date = api.json()
-    # print(date)
+
     return date
